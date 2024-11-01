8df667a989185b77c0fa4546004e7d470059f515
mictlanhalley <97858954+mictlanhalley@users.noreply.github.com>
2024-10-31 20:41:07 -0600

    bug

--- src/pages/home/header/card/Card.jsx
+++ src/pages/home/header/card/Card.jsx
@@ -1,12 +1,13 @@
 import React, { useEffect, useState } from 'react';
 import css from './card.module.scss';
 import axios from 'axios';
-import { URL_ESPECIES, URL_POKEMON } from '../../../../api/apiRest';
+import { URL_ESPECIES, URL_EVOLUCIONES, URL_POKEMON } from '../../../../api/apiRest';
 
 export default function Card({ card }) {
 
-  const [itemPokemon, setItemPokemon] = useState({})
-  const [especiePokemon, setEspeciePokemon] = useState({})
+  const [itemPokemon, setItemPokemon] = useState({});
+  const [especiePokemon, setEspeciePokemon] = useState({});
+  const [evoluciones, setEvoluciones] = useState({});
 
   console.log(itemPokemon);
 
@@ -22,35 +23,106 @@ export default function Card({ card }) {
   
     dataPokemon();
 
-  }, []);
+  }, [card]);
 
   
+
+
   useEffect(() => {
-    const especiePokemon = async () => {
+    const dataEspecie = async () => {
       const URL = card.url.split("/");
       // Usa 'await' para esperar la respuesta de la API
       const api = await axios.get(`${URL_ESPECIES}/${URL[6]}`);
-      setEspeciePokemon(api.data)
+      setEspeciePokemon({
+        url_especie: api?.data?.evolution_chain,
+        data: api?.data,
+      })
     };
-    especiePokemon();
+    dataEspecie();
     
-  }, []);
+  }, [card]);
+
+
+  useEffect(() => {
+    async function getPokemonImagen(id){
+      const response = await axios.get(`${URL_POKEMON}/ ${id}`)
+      return response?.data?.sprites?.other["official-atwork"]?.front_default;
+    }
+
+    
+    if (especiePokemon.url_especie) {
+      
+      const obtenerEvoluciones = async () => {
+        const arrayEvoluciones = [] 
+        const URL = especiePokemon?.url_especie?.url.split("/");
+        // Usa 'await' para esperar la respuesta de la API
+        const api = await axios.get(`${URL_EVOLUCIONES}/${URL[6]}`);
+        
+        const URL2 = api?.data?.chain?.species?.url?.split("/");
+        const img1 = await getPokemonImagen(URL2[6])
+        
+        
+        arrayEvoluciones.push({
+          img: img1,
+          name: api?.data?.chain?.species?.name,
+        })
+        
+        
+        if (api?.data?.chain.evolves_to?.length === 0) {
+          
+          const DATA2 = api?.data?.chain.evolves_to[0] ?. species;
+          const ID = DATA2.url.split("/")
+          const img2 = await getPokemonImagen(ID[6]);
+
+          arrayEvoluciones.push({
+            img: img2,
+            name: DATA2?.name,
+          });
+
+          if (api?.data?.chain.evolves_to[0].evolves_to.length === 0) {
+            const DATA3 = api?.data?.chain?.evolves_to[0]?.evolves_to[0]?.species
+            const ID = DATA3.url.split("/")
+            const img3 = await getPokemonImagen(ID[6]);
   
-  
-  
-  
-  
-  console.log(especiePokemon?.color?.name);
+            arrayEvoluciones.push({
+              img: img3,
+              name: DATA3?.name,
+            });
+
+          }
+
+
+
+
+        }
+        
+        setEvoluciones(arrayEvoluciones);
+        
+      };
+      
+      
+      obtenerEvoluciones();
+    }
+  }, [especiePokemon]);
+
+  let pokeId = itemPokemon?.id?.toString();
+
+    if (pokeId?.length === 1) {
+      pokeId = "00" + pokeId ;
+    } else if(pokeId?.length === 2){
+      pokeId = "0" + pokeId ;
+    }
+
   return (
     <div className={css.card}>
       <img className={css.img_poke} src={itemPokemon?.sprites?.other["official-artwork"]?.front_default} alt="pokemon" 
       />
-      <div className={`bg-${especiePokemon?.color?.name} ${css.sub_card}  `  }  >
-        <strong className={css.id_card}> 011 </strong>
-        <strong className={css.name_card}> name </strong>
-        <h4 className={css.altura_poke}>10cms</h4 >
-        <h4 className={css.peso_poke}>peso</h4>
-        <h4 className={css.habitat_poke}>habitat</h4>
+      <div className={`bg-${especiePokemon?.data?.color?.name} ${css.sub_card}  `  }  >
+        <strong className={css.id_card}> {pokeId} </strong>
+        <strong className={css.name_card}> {itemPokemon.name} </strong>
+        <h4 className={css.altura_poke}>altura: {itemPokemon.height}0 cm</h4 >
+        <h4 className={css.peso_poke}>peso: {itemPokemon.weight} kg</h4>
+        <h4 className={css.habitat_poke}>Habitat: {especiePokemon?.data?.habitat?.name}</h4>
 
         <div className={css.div_stats}>
           {itemPokemon?.stats?.map((sta, index) => {
@@ -65,12 +137,20 @@ export default function Card({ card }) {
         </div>
 
 
-          <div>
+          <div className={css.div_type_color}>
             {itemPokemon?.types?.map((ti , index) =>{
-              return<h6 key={index}>
-                {ti.type.name}
-              </h6>;
-              
+              return(<h6 key={index} className={`color-${ti.type.name} ${css.color_type}`}> {ti.type.name} </h6>
+              );
+            })}
+          </div>
+          <div>
+            {evoluciones?.map((evo )=> {
+              return(
+                <div>
+                  <img src={evo.name} alt="" />
+                  <h6> {evo.name} </h6>
+                </div>
+              )
             })}
           </div>
       </div>
