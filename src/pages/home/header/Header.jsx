0acff4325ff38c4d8ff91b8c8fe1ba7980711b3c
mictlanhalley <97858954+mictlanhalley@users.noreply.github.com>
2024-11-01 22:07:05 -0600

    pokeapi-terminada

--- src/pages/home/header/Header.jsx
+++ src/pages/home/header/Header.jsx
@@ -1,16 +1,28 @@
 import React from 'react';
+import * as FaIcons from 'react-icons/fa';
 import css from './header.module.scss';
 import logo from '../../../assets/logo.png';
 
-export default function Header() {
+
+export default function Header({obtenerSearch}) {
+
+
   return (
     <nav className={css.header} >
         <div className={css.div_header}>
             <div className={css.div_logo}>
                 <img src={logo} alt="logo" />
+                
             </div>
             <div className={css.div_serach}>
-                <input type='search'/>
+
+              <div>
+                <FaIcons.FaSearch/>
+              </div>
+                <input
+                type='search'
+                onChange={(e) => obtenerSearch(e.target.value)}
+                />
             </div>
         </div>
 
--- src/pages/layout/LayoutHome.jsx
+++ src/pages/layout/LayoutHome.jsx
@@ -3,31 +3,88 @@ import css from './layout.module.scss';
 import Header from '../home/header/Header';
 import Card from '../home/header/card/Card'; // Importa el componente Card
 import axios from 'axios';
+import * as FaIcons from 'react-icons/fa';
 import { URL_POKEMON } from '../../api/apiRest';
 
 export default function LayoutHome() {
   const [arrayPokemon, setArrayPokemon] = useState([]);
+  const [globalPokemon, setGlobalPokemon] = useState([]);
+  const [xpage, setXpage] = useState(1);
+  const [search, setSearch] = useState('');
+
 
   useEffect(() => {
     const api = async () => {
-      try {
-        const apiPoke = await axios.get(`${URL_POKEMON}`);
-        setArrayPokemon(apiPoke.data.results); // Asegúrate de acceder a 'results' correctamente
-      } catch (error) {
-        console.error("Error al obtener los datos de Pokémon: ", error);
-      }
+      const limit = 15;
+      const xp = (xpage - 1) * limit;
+      
+        const apiPoke = await axios.get(`${URL_POKEMON}/?offset=${xp}&limit=${limit}`);
+        setArrayPokemon(apiPoke.data.results);
     };
-
     api();
-  }, []);
+    getGlobalPokemons()
+  }, [xpage]);
+
+  const getGlobalPokemons = async () => {
+    const res = await axios.get(`${URL_POKEMON}?offset=0&limit=1000`);
+    const promises = res.data.results.map (pokemon => {
+      return pokemon ;
+    });
+
+    const results = await Promise.all(promises);
+    setGlobalPokemon(results);
+
+  };
+
+  const filterPokemons = search?.length > 0 
+  ? globalPokemon?.filter(pokemon => pokemon?.name?.includes(search))
+  : arrayPokemon
+
+  const obtenerSearch = (e) => {
+
+    const texto = e.toLowerCase()
+    setSearch(texto)
+    setXpage(1)
+  }
 
   return (
     <div className={css.layout}>
-      <Header />
+      <Header obtenerSearch={obtenerSearch} />
+
+      <section className={css.section_pagination}>
+        <div className={css.div_pagination}>
+          <span className={css.item_izquierdo} 
+          onClick={() => {
+            if (xpage === 1) {
+              return console.log("no puedo retroceder");
+            }
+            setXpage(xpage - 1);
+          }}
+          >
+            <FaIcons.FaAngleLeft />
+          </span>
+          <span className={css.item}>{xpage}</span>
+          <span className={css.item}>DE</span>
+          <span className={css.item}>
+            {" "}
+            {Math.round(globalPokemon?.length / 15)}{" "}
+            </span>
+          <span className={css.item_derecho}
+          onClick={() => {
+            if (xpage === 67) {
+              return console.log("es el ultimo");
+            }
+            setXpage(xpage + 1);
+          }}
+          >
+            {" "}
+            <FaIcons.FaAngleRight />{" "}
+          </span>
+        </div>
+      </section>
 
       <div className={css.card_content}>
-        {arrayPokemon.map((card, index) => (
-          // Renderiza el componente Card y pasa los datos de cada Pokémon
+        {filterPokemons.map((card, index) => (
           <Card key={index} card={card} />
         ))}
       </div>
